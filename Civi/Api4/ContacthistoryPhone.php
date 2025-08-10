865e756ebbbb7b353ff6934061b7ba627c7843b7
kenmoellman <49815696+kenmoellman@users.noreply.github.com>
2025-08-10 02:29:33 -0400

    Create ContacthistoryPhone.php

---
+++ Civi/Api4/ContacthistoryPhone.php
@@ -0,0 +1,16 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * ContacthistoryPhone entity.
+ *
+ * Provided by the Contact History extension.
+ *
+ * @searchable primary
+ * @since 5.19
+ * @package Civi\Api4
+ */
+class ContacthistoryPhone extends Generic\DAOEntity {
+  use Generic\Traits\ManagedEntity;
+
+}
