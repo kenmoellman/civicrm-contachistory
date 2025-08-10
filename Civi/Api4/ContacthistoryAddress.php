905d5a8992def53bd5d7e216b112e64b07b42369
kenmoellman <49815696+kenmoellman@users.noreply.github.com>
2025-08-10 02:28:38 -0400

    Create ContacthistoryAddress.php

---
+++ Civi/Api4/ContacthistoryAddress.php
@@ -0,0 +1,16 @@
+<?php
+namespace Civi\Api4;
+
+/**
+ * ContacthistoryAddress entity.
+ *
+ * Provided by the Contact History extension.
+ *
+ * @searchable primary
+ * @since 5.19
+ * @package Civi\Api4
+ */
+class ContacthistoryAddress extends Generic\DAOEntity {
+  use Generic\Traits\ManagedEntity;
+
+}
