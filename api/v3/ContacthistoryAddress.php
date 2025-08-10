778f5c50f60da371575ca9940301b4270453f680
kenmoellman <49815696+kenmoellman@users.noreply.github.com>
2025-08-10 02:26:58 -0400

    Create ContacthistoryAddress.php

---
+++ api/v3/ContacthistoryAddress.php
@@ -0,0 +1,56 @@
+<?php
+use CRM_Contacthistory_ExtensionUtil as E;
+
+/**
+ * ContacthistoryAddress.create API specification (optional).
+ * This is used for documentation and validation.
+ *
+ * @param array $spec description of fields supported by this API call
+ *
+ * @see https://docs.civicrm.org/dev/en/latest/framework/api-architecture/
+ */
+function _civicrm_api3_contacthistory_address_create_spec(&$spec) {
+  $spec['contact_id']['api.required'] = 1;
+}
+
+/**
+ * ContacthistoryAddress.create API.
+ *
+ * @param array $params
+ *
+ * @return array
+ *   API result descriptor
+ *
+ * @throws API_Exception
+ */
+function civicrm_api3_contacthistory_address_create($params) {
+  return _civicrm_api3_basic_create(_civicrm_api3_get_BAO(__FUNCTION__), $params, 'ContacthistoryAddress');
+}
+
+/**
+ * ContacthistoryAddress.delete API.
+ *
+ * @param array $params
+ *
+ * @return array
+ *   API result descriptor
+ *
+ * @throws API_Exception
+ */
+function civicrm_api3_contacthistory_address_delete($params) {
+  return _civicrm_api3_basic_delete(_civicrm_api3_get_BAO(__FUNCTION__), $params);
+}
+
+/**
+ * ContacthistoryAddress.get API.
+ *
+ * @param array $params
+ *
+ * @return array
+ *   API result descriptor
+ *
+ * @throws API_Exception
+ */
+function civicrm_api3_contacthistory_address_get($params) {
+  return _civicrm_api3_basic_get(_civicrm_api3_get_BAO(__FUNCTION__), $params, TRUE, 'ContacthistoryAddress');
+}
