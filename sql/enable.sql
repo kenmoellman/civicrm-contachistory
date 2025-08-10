b4d5508b176daf7cd229c5a8dc981fda9d1490f3
kenmoellman <github@mu-net.org>
2025-08-10 05:42:18 -0400

    Initial commit

--- sql/enable.sql
+++ sql/enable.sql
@@ -1,4 +1,4 @@
--- Insert current data into history tables (using only core fields that exist in all installations)
+-- Insert current data into history tables (only if history tables are empty)
 INSERT INTO civicrm_contacthistory_address (
   original_id, contact_id, location_type_id, is_primary, is_billing, 
   street_address, supplemental_address_1, supplemental_address_2, 
@@ -10,7 +10,8 @@ SELECT
   street_address, supplemental_address_1, supplemental_address_2,
   city, state_province_id, postal_code, country_id,
   geo_code_1, geo_code_2, manual_geo_code
-FROM civicrm_address;
+FROM civicrm_address
+WHERE NOT EXISTS (SELECT 1 FROM civicrm_contacthistory_address LIMIT 1);
 
 INSERT INTO civicrm_contacthistory_email (
   original_id, contact_id, location_type_id, email, is_primary, is_billing,
@@ -19,7 +20,8 @@ INSERT INTO civicrm_contacthistory_email (
 SELECT 
   id, contact_id, location_type_id, email, is_primary, is_billing,
   on_hold, is_bulkmail, hold_date, reset_date
-FROM civicrm_email;
+FROM civicrm_email
+WHERE NOT EXISTS (SELECT 1 FROM civicrm_contacthistory_email LIMIT 1);
 
 INSERT INTO civicrm_contacthistory_phone (
   original_id, contact_id, location_type_id, is_primary, is_billing,
@@ -28,5 +30,6 @@ INSERT INTO civicrm_contacthistory_phone (
 SELECT 
   id, contact_id, location_type_id, is_primary, is_billing,
   phone, phone_ext, phone_numeric, phone_type_id
-FROM civicrm_phone;
+FROM civicrm_phone
+WHERE NOT EXISTS (SELECT 1 FROM civicrm_contacthistory_phone LIMIT 1);
 
