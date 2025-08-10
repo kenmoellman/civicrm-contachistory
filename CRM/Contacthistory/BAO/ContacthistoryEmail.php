23a00b2cc6fe4ef8f456badb7d10ba03f0116cc0
kenmoellman <49815696+kenmoellman@users.noreply.github.com>
2025-08-10 02:22:32 -0400

    Create ContacthistoryEmail.php

---
+++ CRM/Contacthistory/BAO/ContacthistoryEmail.php
@@ -0,0 +1,60 @@
+<?php
+use CRM_Contacthistory_ExtensionUtil as E;
+
+class CRM_Contacthistory_BAO_ContacthistoryEmail extends CRM_Contacthistory_DAO_ContacthistoryEmail {
+
+  /**
+   * Create a new ContacthistoryEmail based on array-data
+   *
+   * @param array $params key-value pairs
+   * @return CRM_Contacthistory_DAO_ContacthistoryEmail|NULL
+   */
+  public static function create($params) {
+    $className = 'CRM_Contacthistory_DAO_ContacthistoryEmail';
+    $entityName = 'ContacthistoryEmail';
+    $hook = empty($params['id']) ? 'create' : 'edit';
+
+    CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
+    $instance = new $className();
+    $instance->copyValues($params);
+    $instance->save();
+    CRM_Utils_Hook::post($hook, $entityName, $instance->id, $instance);
+
+    return $instance;
+  }
+
+  /**
+   * Get history count for a contact
+   *
+   * @param int $contactId
+   * @return int
+   */
+  public static function getHistoryCount($contactId) {
+    $sql = "SELECT COUNT(*) FROM civicrm_contacthistory_email WHERE contact_id = %1";
+    return CRM_Core_DAO::singleValueQuery($sql, [1 => [$contactId, 'Integer']]);
+  }
+
+  /**
+   * Get history for a contact
+   *
+   * @param int $contactId
+   * @return array
+   */
+  public static function getHistory($contactId) {
+    $sql = "
+      SELECT h.*, lt.display_name as location_type
+      FROM civicrm_contacthistory_email h
+      LEFT JOIN civicrm_location_type lt ON h.location_type_id = lt.id
+      WHERE h.contact_id = %1
+      ORDER BY h.start_date DESC, h.id DESC
+    ";
+    
+    $dao = CRM_Core_DAO::executeQuery($sql, [1 => [$contactId, 'Integer']]);
+    $results = [];
+    while ($dao->fetch()) {
+      $results[] = $dao->toArray();
+    }
+    return $results;
+  }
+
+}
